295517a109b1951b70b44f487f5697789ce705cb
PEN <uemasaki20011222@gmail.com>
2025-08-28 19:52:09 +0900

    init

--- linux_data/ruby/app/helpers/users_helper.rb
+++
@@ -1,9 +0,0 @@
-module UsersHelper
-
-    # 引数で与えられたユーザーの画像を返す
-    def user_img(user)
-        imgId = "default.ico"
-        image_tag(imgId, alt: user.name, class: "userImg", width: "80px", height: "80px")
-    end
-end
-  
