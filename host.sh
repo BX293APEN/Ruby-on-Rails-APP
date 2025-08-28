4bd62cda35526016ba93d740b8bb57c37148c2e5
PEN <uemasaki20011222@gmail.com>
2025-08-28 23:21:30 +0900

    修正

--- host.sh
+++ host.sh
@@ -1,2 +1,17 @@
-# docker compose run web rake db:create # 初回のみ実行
+#!/bin/bash
+
+# .envから値を読み込み
+set -a
+source .env
+set +a
+
+DB_FILE="${VOLUME}/ruby/db/development.sqlite3" 
+
+if [ -f "$DB_FILE" ]; then
+  echo "SQLite DB file exists at $DB_FILE. Skipping db:create."
+else
+  echo "SQLite DB file not found. Running 'docker compose run web rake db:create'..."
+  docker compose run web rake db:create
+fi
+
 docker compose up --build -d
